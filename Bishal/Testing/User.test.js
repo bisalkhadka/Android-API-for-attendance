feb46656176f427fad8328b3d24ebe93c2ed277c
Your Name <you@example.com>
2019-07-09 02:08:03 +0545

    CRUD function testing done for every modules.

--- Bishal/Testing/User.test.js
+++ Bishal/Testing/User.test.js
@@ -1,62 +1,56 @@
 const User = require('../model/User');
- const mongoose = require('mongoose');
- const url = 'mongodb://localhost:27017/Testing'; // use the new name of the database 
- 
-beforeAll(async () => {  
-       await mongoose.connect(url, {         
-           useNewUrlParser: true, 
-        useCreateIndex: true     }); }); 
- 
-afterAll(async () => { 
- 
-    await mongoose.connection.close(); }); 
- 
-describe('User  Schema test', () => {    
-     it('Add User testing', () => {         
-         const user = {  
-             'Firstname': 'Bishal',
-             'Lastname' : 'Khadka' , 
-             'Email' : 'bisal11@gmail.com' ,
-             'Username': 'Bisal' ,  
-             'Address' : 'KTM'   ,
-             'Module' : 'Science',
-             'Password' : 'softwarica',
-             'Confpassword' : 'softwarica'
-             };                  
-             return User.create(user)             
-             .then((users) => {                 
-                 expect(users.Firstname).toEqual('Bishal'); 
-                 expect(users.Lastname).toEqual('Khadka');  
-                 expect(users.Email).toEqual('bisal11@gmail.com');  
-                 expect(users.Username).toEqual('Bisal');  
-                 expect(users.Address).toEqual('KTM');  
-                 expect(users.Module).toEqual('Science');  
-                 expect(users.Password).toEqual('softwarica');  
-                 expect(users.Confpassword).toEqual('softwarica');             
-                 });
-                     }); 
+const mongoose = require('mongoose');
+const url = 'mongodb://localhost:27017/TestUserattendance';
+beforeAll(async () => {
+   await mongoose.connect(url, {
+      useNewUrlParser: true,
+      useCreateIndex: true
+   });
+});
+afterAll(async () => {
+   await mongoose.connection.close();
+});
 
+describe('Attendance System User testing', () => {
+   var id='';
+   // adding a user
+   it('User register testing', () => {
+      const user = {
+         Firstname: 'Vishal',
+         Lastname: 'Khadka Chhetri',
+         Email: 'bisalkhadka@gmail.com',
+         Username: 'bisal',
+         Address: 'Maitidevi chowk',
+         Module: 'Science',
+         Password: 'bisal',
+         Confpassword:'bisal',
+         usertype: 'user',
+         fileToUpload: 'Student.jpg'
+      };
 
-                      //update user
-        it('to test the update', async () => {
+      return User.create(user)
+         .then((user_res) => {
+            id=user_res._id;
+            expect(user_res.Username).toEqual('bisal');
+         });
+   });
 
-            return User.findOneAndUpdate({ _id: Object('5d1a02854206b00fc0420b3d') },
-                { $set: { Username: 'bisal' } }).then((users) => {
-                    expect(users.Username).toEqual('Bishal')
-                })
-        });
+   //update user
+   it('updateuser testing', () => {
+     
+      const userup = {
+         
+         Username: 'bishal1234'
+      }
+      console.log(id)
+      return User.findByIdAndUpdate(id,userup,{ new: true })
+         .then((userupd) => {
+            expect(userupd.Username).toEqual('bishal1234');
+         });
+   });
 
-
-
-        it('to test the delete product is working or not', async () => {
-        const status = await Product.deleteMany();  
-        expect(status.ok).toBe(1); });     
-        
-        
-        
-    });
- 
+  
     
+   });
 
-
-
+      
